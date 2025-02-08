7e4faeff7b72ed20d7af7922f4f84e79a91ff7d8
Eric Kwan <kwan.eric69@gmail.com>
2025-02-07 22:53:21 -0500

    made highlights to consonants only.

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
-console.log({Developer: "Eric Kwan", date: new Date()})
+"use strict";
 
-let students = [
-    {emplid: "1000", score : 95},
-    {emplid: "2000", score : 85},
-    {emplid: "3000", score : 75},
-    {emplid: "4000", score : 65}];
+function updateRowCount() {
+    let divSelections = document.querySelectorAll("div.abc");
+    console.log({lengthByDeveloper : divSelections.length});
+    let span = document.getElementById("rowCount");
+    span.innerHTML = divSelections.length;
+}
 
-    function getGPA(score) {
-        let myGrade = "F";
-    
-        if (score > 92)
-            myGrade = "A";
-        else if (score > 82)
-            myGrade = "B";
-        else if (score > 70)
-            myGrade = "C";
-        else if (score < 70)
-            myGrade = "D";
-    
-        return myGrade;
-    }
-    
-    
-    let grade = getGPA(students[0].score);
-    students[0].letterGrade = grade;
-    
-    grade = getGPA(students[1].score);
-    students[1].letterGrade = grade;
-    
-    grade = getGPA(students[2].score);
-    students[2].letterGrade = grade;
-    
-    grade = getGPA(students[3].score);
-    students[3].letterGrade = grade;
-    
+function highLight() {
+    let nodeSelections = document.querySelectorAll("div.abc");
+    let divSelectionArray = [...nodeSelections];
 
-console.log({students});
+    divSelectionArray.forEach(element => {
+        let text = element.innerHTML;
+
+        if (isConsonant(text)){
+            element.classList.add("highLight");
+        }
+    });
+}
+
+function isVowel(text) {
+    let pattern = /[AEIOUaeiou]/;
+    let result = pattern.test(text);
+    return result;
+}
+
+function isConsonant(text){
+    return !isVowel(text);
+}
+
+updateRowCount();
